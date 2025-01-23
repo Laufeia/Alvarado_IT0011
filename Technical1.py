ea40ff7c76bfc2073e6f07316387794c4fd0f14d
Laufeia <roguespy25@gmail.com>
2025-01-23 11:33:47 +0800

    Technical1

---
+++ Technical1.py
@@ -0,0 +1,19 @@
+Fname = input("Enter Firstname: ")
+Lname = input("Enter Lastname: ")
+Mname = input("Enter Middlename: ")
+Birthday = input("Enter Birthday: ")
+Gender = input("Enter Gender: ")
+Address = input("Enter Address: ")
+Course = input("Enter Course: ")
+Yearlevel = input("Enter Yearlevel: ")
+
+print("======================================================================")
+print("FirstName:",Fname, 
+      "\nLastname: ",Lname,
+      "\nMiddlename: ",Mname,
+      "\nBirthday: ",Birthday,
+      "\nGender: ",Gender,
+      "\nAddress: ",Address,
+      "\nCourse: ",Course,
+      "\nYearlevel: ",Yearlevel)
+print("======================================================================")

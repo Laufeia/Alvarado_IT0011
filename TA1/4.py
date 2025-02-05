bc3f7d125ebc328a25b492770c5554e1eb1ad891
Laufeia <roguespy25@gmail.com>
2025-02-06 04:35:46 +0800

    A

--- TA1/4.py
+++ TA1/4.py
@@ -1,10 +1,7 @@
-n = 7
+n = 7 
 i = 1 
 
 while i <= n:
-    j = 1
-    while j <= i:
-        print(i, end="")
-        j += 1
-    print() 
-    i += 2
+    if i % 2 != 0 or i == 6:
+        print(str(i) * i)
+    i += 1 
